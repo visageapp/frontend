4ad86373bd9ff8cbae5892bdf4a0646b51deea4b
Kennet Postigo <kennetfpostigo@gmail.com>
2016-04-17 03:04:57 -0400

    card fixes, making card beautiful

--- app/components/App.js
+++ app/components/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Devtools from './Devtools/Devtools';
 import styles from './style.css';
-import { RouteTransition } from 'react-router-transition';
 
 export default class App extends React.Component{
 
