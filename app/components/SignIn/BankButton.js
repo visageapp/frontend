db4980793d4ef0b6c6829ab4cf8c1e803d13d2f3
Kennet Postigo <kennetfpostigo@gmail.com>
2016-04-17 00:27:35 -0400

    style tweaks on card and list for banks

--- app/components/SignIn/BankButton.js
+++ app/components/SignIn/BankButton.js
@@ -3,6 +3,6 @@ export default class BankButton extends React.Component {
 
   render() {
     var lc = this.props.bank.replace(/\s+/g, '').toLowerCase();
-    return (<div style={{width: 128}}><img src={`../assets/banks/${lc}.svg`} alt={this.props.bank}/></div>);
+    return (<div style={{height:'30px', width: 'auto', color: '#000'}}><img style={{height:'90%', width: 'auto'}} src={`../assets/banks/${lc}.svg`} alt={this.props.bank}/></div>);
   }
 }
